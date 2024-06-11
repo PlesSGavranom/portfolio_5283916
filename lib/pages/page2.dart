151addb849d075f6df07571dec3b621fce7dd009
Daniel Stiel <daniel_stiel@yahoo.de>
2024-06-11 11:26:04 +0200

    Textanpassungen

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -8,11 +8,27 @@ class Page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: const Center(
-        child: Text(
-          'Seite 2',
-          style: TextStyle(fontSize: 24, color: Colors.white),
-        ),
+      padding: EdgeInsets.all(16),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Zu meiner Person: Daniel Stiel',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Neben meiner unfassbaren Leidenschaft für das Fach: \n'
+                '"Entwicklung Grafischer Benutzeroberflächen" verbringe ich '
+                'viel Zeit mit Counter Strike, Bitchez und Drogen',
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Folge mir auf Insta: @daniel_stiel',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
