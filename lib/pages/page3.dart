4b9db86b3a0a1d8cfa3d4a67591bb0a686470573
Daniel Stiel <daniel_stiel@yahoo.de>
2024-06-11 13:32:57 +0200

    Textanpassungen

--- lib/pages/page3.dart
+++ lib/pages/page3.dart
@@ -5,14 +5,61 @@ class Page3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: const Center(
-        child: Text(
-          'Seite 3',
-          style: TextStyle(fontSize: 24, color: Colors.white),
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Daniel Stiel',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Geburtsdatum: 06.06.1999\n',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Ausbildung:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Grundschule am Hasenberg',
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Christian-Wirth-Schule',
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Technische Hochschule Mittelhessen MND',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Berufserfahrung:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Client Account Manager - Allianz Global Investors (07/24 - heute)',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
         ),
       ),
-    );
+    ),
+      );
   }
 }
